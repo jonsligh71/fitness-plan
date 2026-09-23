dbfc28c7b007d28f721b2e8890d4e46daace7cd0
jonsligh71 <jonsligh@gmail.com>
2026-09-22 22:49:38 -0400

    Bump app cache for guided training update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-plan-v2";
+const CACHE = "fitness-plan-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
