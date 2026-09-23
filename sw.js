53341750b46ddf031912e9b3b72aefaac608f3e4
jonsligh71 <jonsligh@gmail.com>
2026-09-22 23:19:36 -0400

    Bump cache for exercise identity UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-plan-v8";
+const CACHE = "fitness-plan-v9";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
