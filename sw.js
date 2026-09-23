ae6e0a70dd07af377c522a149e2d7ac1ca65e63e
jonsligh71 <jonsligh@gmail.com>
2026-09-22 23:06:51 -0400

    Bump cache for compact workout UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-plan-v6";
+const CACHE = "fitness-plan-v7";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
