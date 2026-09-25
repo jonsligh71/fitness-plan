32a05d0872a247f82b97445e2b63499ef3e93b96
jonsligh71 <jonsligh@gmail.com>
2026-09-24 23:53:36 -0400

    Publish v16 redesigned onboarding and exercise visuals

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-plan-v15";
+const CACHE = "fitness-plan-v16";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
