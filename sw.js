9ab1595629b6f1237b8dc736720abcf1139dd63f
jonsligh71 <jonsligh@gmail.com>
2026-09-24 23:55:33 -0400

    Publish premium home redesign v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-plan-v16";
+const CACHE = "fitness-plan-v17";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
