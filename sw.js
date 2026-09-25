c7ce0794bf18cfb38e30ce24bc8c33c8c8024ef4
jonsligh71 <jonsligh@gmail.com>
2026-09-25 00:18:06 -0400

    Publish v22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-plan-v21";
+const CACHE = "fitness-plan-v22";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
