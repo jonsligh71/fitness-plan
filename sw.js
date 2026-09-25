5d03a0a16bdde5eb621c363d5de36d71bb0408a8
jonsligh71 <jonsligh@gmail.com>
2026-09-25 00:23:52 -0400

    Publish v26 spacing correction

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-plan-v25";
+const CACHE = "fitness-plan-v26";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
